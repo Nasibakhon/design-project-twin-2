a49060d02d5665b8a0ee4ed01a100dfebc658f59
Nasibakhon <34415443+Nasibakhon@users.noreply.github.com>
2018-05-14 09:59:14 +0500

    adding

--- about.php
+++ about.php
@@ -9,7 +9,7 @@
         	<?php
 			
             echo '<h1>About</h1>
-          <h2>Internet Programming Project</h2>
+          <h2>Internet Programming Project </h2>// adding commnet
 		  <p>In this site you can find and download the books which you like </p>
 		  Made by : Elchieva Nasibakhon (u1610164), Tulasheva Gulkhayokhon (u1610074)
         </div>'; ?> 
